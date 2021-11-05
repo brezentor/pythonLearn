34cf1c8ed72a997dd520808d802eb4e8ae44792d
Maxim Biloborodko <berzerker994@gmail.com>
2021-11-05 19:03:01 +0200

    9

--- url.py
+++ url.py
@@ -1,7 +1,14 @@
 from urllib import request
 
-myurl = "http://10.30.0.77/admin/spacfg.xml"
-answer = request.urlopen(myurl)
+#tip = "D:\\python\\trueresults.log"
+#iptest = open(tip, mode="r")
+#for p in iptest:
+  #ipaddrs = str(p)
+
+ipaddrs = "10.30.0.77"
+myurl = "http://{ip}/admin/spacfg.xml"
+mu = myurl.format(ip = ipaddrs)
+answer = request.urlopen(mu)
 ans = answer.readlines()
 a = ans[13]
 mac = str(a).lstrip("""b'<MAC_Address group="Info/Product_Information">""").rstrip("n'").rstrip("</MAC_Address>\\")
