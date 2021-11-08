f3867fc80a2896069e061b1a1e4d72b144e5810d
Maxim Biloborodko <berzerker994@gmail.com>
2021-11-08 16:55:56 +0200

    9.3

--- 15.Try_Except.py
+++ 15.Try_Except.py
@@ -2,7 +2,7 @@ from urllib import request
 import sys
 
 writeerr = open(file = "D:\\python\\pythonLearn\\15.Try_Except.txt", mode = "a")
-ipaddrs = "10.30.0.66"
+ipaddrs = "10.30.0.120"
 myurl = "http://{ip}/admin/spacfg.xml"
 mu = myurl.format(ip=ipaddrs)
 try:
@@ -15,6 +15,6 @@ else:
     ans = answer.readlines()
     a = ans[13]
     mac = str(a).lstrip("""b'<MAC_Address group="Info/Product_Information\"""").lstrip(">").rstrip("n'").rstrip(
-        "</MAC_Address>\\")
+        "/MAC_Address>\\").rstrip("<")
     print(a)
     print(mac)
--- url.py
+++ url.py
@@ -1,25 +1,36 @@
 from urllib import request
 import sys
 
-#tip = "D:\\python\\trueresults.log"
-#iptest = open(tip, mode="r")
-#for p in iptest:
-  #ipaddrs = str(p)
+arpres = "D:\\python\\arp.log"
+tip = "D:\\python\\trueresults.log"
+arper = "D:\\python\\arp_error.log"
 
-writeerr = open(file = "D:\\python\\pythonLearn\\15.Try_Except.txt", mode = "a")
-ipaddrs = "10.30.0.66"
-myurl = "http://{ip}/admin/spacfg.xml"
-mu = myurl.format(ip=ipaddrs)
-try:
-    answer = request.urlopen(mu)
-except:
-    print(sys.exc_info()[1])
-    writeerr.write(ipaddrs + "\n")
-    writeerr.close()
-else:
-    ans = answer.readlines()
-    a = ans[13]
-    mac = str(a).lstrip("""b'<MAC_Address group="Info/Product_Information\"""").lstrip(">").rstrip("n'").rstrip(
-        "</MAC_Address>\\")
-    print(a)
-    print(mac)
+purgarp = open(arpres, mode = "w")
+purgarper = open(arper, mode = "w")
+purgarp.close()
+purgarper.close()
+
+writeerr = open(arper, mode = "a")
+iptest = open(tip, mode="r")
+arpresults = open(arpres, mode = "a")
+
+for p in iptest:
+    ipaddrs = str(p).rstrip(' \n')
+    myurl = "http://{ip}/admin/spacfg.xml"
+    mu = myurl.format(ip=ipaddrs)
+    try:
+        answer = request.urlopen(mu)
+    except:
+        print(sys.exc_info()[1])
+        writeerr.write(ipaddrs + "\n")
+    else:
+        ans = answer.readlines()
+        a = ans[13]
+        mac = str(a).lstrip("""b'<MAC_Address group="Info/Product_Information\"""").lstrip(">").rstrip("n'").rstrip(
+        "/MAC_Address>\\").rstrip("<")
+        print(a)
+        arpresults.write(mac + " - " + ipaddrs + "\n")
+
+writeerr.close()
+iptest.close()
+arpresults.close()
